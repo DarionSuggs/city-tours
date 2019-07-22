c235ee4da5241661c95753569a55427056786aa7
dreadedDev <dreadeddev@Darions-MacBook-Pro.local>
2019-07-22 01:24:49 -0500

    first commit

---
+++ src/tourData.js
@@ -0,0 +1,34 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "milwaukee",
+    img: "./img/Statue of The Fonz.jpg",
+    name: "Statue of The Fonz",
+    info:
+      "The Bronze Fonz is a public artwork by American artist Gerald P. Sawyer located on the Milwaukee Riverwalk just south of Wells Street in downtown Milwaukee. The Bronze Fonz depicts Henry Winkler as the Happy Days TV show character Arthur Fonzarelli, also known as the Fonz. Fonzie stands in his typical costume, which consists of a leather jacket and jeans. He is giving a two-handed thumbs up. "
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "./img/paris.jpeg",
+    name: "paris lights tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  },
+  {
+    id: 3,
+    city: "london",
+    img: "./img/london.jpeg",
+    name: "london royal palace tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "./img/tokyo.jpeg",
+    name: "tokyo sushi tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  }
+];
